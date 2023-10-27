1a9bfef0f7b0751e69fd8d58edb4d5f031ae26ce
HongJungWan <hjw43ok@hs.ac.kr>
2023-10-27 15:00:04 +0900

    feat: main page - banner 추가

---
+++ src/components/HomePage/HomeBanner.vue
@@ -0,0 +1,62 @@
+<template>
+    <swiper class="mb-3 swiper" :options="swiperOption">
+        <swiper-slide>
+            <v-sheet width="100%" height="250px" style="cursor: pointer">
+                <home-banner-one></home-banner-one>
+            </v-sheet>
+        </swiper-slide>
+        <swiper-slide>
+            <v-sheet width="100%" height="250px" style="cursor: pointer">
+                <home-banner-two></home-banner-two>
+            </v-sheet>
+        </swiper-slide>
+        <swiper-slide>
+            <v-sheet width="100%" height="250px" style="cursor: pointer">
+                <home-banner-three></home-banner-three>
+            </v-sheet>
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+</template>
+
+<script>
+import HomeBannerOne from "./element/HomeBannerOne.vue";
+import HomeBannerTwo from "./element/HomeBannerTwo.vue";
+import HomeBannerThree from "./element/HomeBannerThree.vue";
+
+export default {
+    name: "HomeBanner",
+    components: {
+        HomeBannerOne,
+        HomeBannerTwo,
+        HomeBannerThree,
+    },
+    data() {
+        return {
+            swiperOption: {
+                slidesPerView: "auto",
+                spaceBetween: 25,
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                },
+                loop: true,
+                autoplay: {
+                    delay: 3000,
+                    disableOnInteraction: false,
+                },
+            },
+        };
+    },
+};
+</script>
+
+<style scoped>
+.swiper-slide {
+    width: 100%;
+}
+
+.swiper-container {
+    width: 100% !important;
+}
+</style>
---
+++ src/components/HomePage/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-sheet class="d-flex flex-column">
+        <home-banner></home-banner>
+
+        <!-- nav 자리 -->
+        <v-sheet height="56px"></v-sheet>
+    </v-sheet>
+</template>
+
+<script>
+import HomeBanner from "./HomeBanner.vue";
+
+export default {
+    name: "HomePage",
+    components: {
+        HomeBanner,
+    },
+    methods: {
+        openBottomSheet() {
+            this.$refs.bottomsheet.open();
+        },
+    },
+};
+</script>
+
+<style>
+</style>
